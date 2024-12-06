fcc623f60a431197a42432e594cdcb340626752c
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-06 23:49:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function mincost(arr)
 { 
 //write your code here
-// return the min cost
-  
+const arr1 = arr.sort(function(a,b){return a-b})
+	let sum=arr1[0]+arr[1]
+	for(let i=2; i<=arr1.length-1; i++){
+       for(let j=0; j<=i; j++){
+		   sum = sum+arr1[j]
+	   }
+	}
+// return the min cost 
+	return sum
 }
-
 module.exports=mincost;
